6fb38e46df2919e955100ad5c6dca0a223837c87
Parvathiv90 <parvathiv.saran@gmail.com>
2026-04-22 11:41:39 +0530

    right section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "../src/css/ass-result.css";
 import "../src/css/howitwork.css";
 import "../src/css/sample.css";
 import "../src/css/WhoItsFor.css";
+import "../src/css/MatchedInstallers.css";
 
 import Footer from "./components/Footer.tsx";
 import Home from "./pages/Home.tsx";
@@ -13,6 +14,7 @@ import AssesementResult from "./pages/AssessmentResult.tsx";
 import HowItWorks from "./pages/HowItWorks.tsx";
 import SampleResults from "./pages/SampleResults.tsx";
 import WhoItsFor from "./pages/WhoItsFor.tsx";
+import MatchedInstallers from "./pages/MatchedInstallers.tsx";
 
 function App() {
   //testgit 08.04.2026
@@ -26,6 +28,7 @@ function App() {
         <Route path="/how-it-works" element={<HowItWorks />} />
         <Route path="/sample-results" element={<SampleResults />} />
         <Route path="/who-its-for" element={<WhoItsFor />} />
+        <Route path="/matched-installers" element={<MatchedInstallers />} />
     
       </Routes>
 
